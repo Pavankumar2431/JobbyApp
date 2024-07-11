011dca81872179299339905a08f1ff91fe453110
Pavankumar2431 <pavankumar99@gmail.com>
2024-07-11 07:27:03 +0000

    solution

--- src/components/ProfileDetails/index.js
+++ src/components/ProfileDetails/index.js
@@ -31,6 +31,7 @@ class ProfileDetails extends Component {
       method: 'GET',
     }
     const response = await fetch(apiUrl, options)
+    console.log(response)
     if (response.ok === true) {
       const data = await response.json()
       const profileData = {
@@ -50,22 +51,20 @@ class ProfileDetails extends Component {
     const {profileData} = this.state
     const {name, profileImageUrl, shortBio} = profileData
     return (
-      <div className="profile-success-container">
-        <img src={profileImageUrl} alt="profile" className="profile-img" />
-        <h1 className="profile-heading">{name}</h1>
-        <p className="profile-bio">{shortBio}</p>
-        <h1 className="profile-heading">Teja Yeshamoni</h1>
-        <p className="profile-bio">Frontend Developer</p>
+      <div className='profile-success-container'>
+        <img src={profileImageUrl} alt='profile' className='profile-img' />
+        <h1 className='profile-heading'>{name}</h1>
+        <p className='profile-bio'>{shortBio}</p>
       </div>
     )
   }
 
   renderFailureView = () => (
-    <div className="profile-error-view-container">
+    <div className='profile-error-view-container'>
       <button
-        type="button"
-        data-testid="button"
-        className="profile-failure-button"
+        type='button'
+        data-testid='button'
+        className='profile-failure-button'
         onClick={this.getProfile}
       >
         Retry
@@ -74,8 +73,8 @@ class ProfileDetails extends Component {
   )
 
   renderLoadingView = () => (
-    <div className="profile-loader-container " data-testid="loader">
-      <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
+    <div className='profile-loader-container ' data-testid='loader'>
+      <Loader type='ThreeDots' color='#ffffff' height='50' width='50' />
     </div>
   )
 
